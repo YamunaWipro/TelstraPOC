e0da31bec3e082f608f52ffa775d2b95ba297387
Yamuna <yamuna.v95@wipro.com>
2018-04-06 11:10:17 +0530

    Intital Commit
    Initial Commit

---
+++ App5/App5/View/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using App5.View;
+using App5.ViewModels;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            
+
+
+            InitializeComponent();
+            Login viewmodel = new Login();
+            BindingContext = viewmodel;
+            ((Login)this.BindingContext).name = usernameEntry.Text;
+                
+            viewmodel._navigation = this.Navigation;
+        }
+        void Handle_Clear_Clicked(object sender, System.EventArgs e)
+        {
+            usernameEntry.Text = string.Empty;
+            passwordEntry.Text = string.Empty;
+        }
+      
+    }
+}
---
+++ App5/App5/ViewModel/Login.cs
@@ -0,0 +1,49 @@
+﻿//using Android.Content.Res;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using App5.View;
+
+namespace App5.ViewModels
+{
+    public class Login : INotifyPropertyChanged
+    {
+       public String name;
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        public INavigation _navigation;
+       
+        public Login()
+        {
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+           
+
+        }
+        async Task ExecuteLoadItemsCommand()
+        {
+            //((m.usernameEntry.Text).Equals(string.Empty))
+            //  await Application.Current.MainPage.DisplayAlert(MainPage.usernameEntry.Text, "Continue", "OK");
+            await _navigation.PushAsync(new NavigationPage(new Page1()));
+            //await Application.Current.MainPage.DisplayAlert("Negotiation", "Saved correctly", "OK");
+
+        }
+        public Command LoadItemsCommand { get ; set; }
+
+
+    }
+}
+    
